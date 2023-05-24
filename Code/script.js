d49bc197399f659c1d9f73e27d5312a516d0b2e5
Kannan <vrindtime@gmail.com>
2023-05-25 00:53:03 +0530

    front page dune

--- Code/script.js
+++ Code/script.js
@@ -1,13 +1,58 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }else{
-            entry.target.classList.remove("show");
-        }
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Define the animations for the "See" section and footer
+    var seeSectionAnimation = gsap.from('.see', {
+        opacity: 0,
+        y: 50,
+        stagger: 0.2,
+        duration: 1,
+        ease: 'power3.out',
+        scrollTrigger: {
+            trigger: '.see',
+            start: 'top 60%', // Adjust the start position as needed
+            srub: true,
+        },
     });
-});
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+    var exAnimation = gsap.from('.ex1, .ex2 ,.ex3 , .ex4', {
+        opacity: 0,
+        x: 50,
+        y: 50,
+        duration: 1,
+        ease: 'power3.out',
+        scrollTrigger: {
+            trigger: '.see',
+            start: 'top 80%',
+            scrub: true, // Adjust the start position as needed
+        },
+    });
+
+    var exAnimation = gsap.from('.skill, .timeline', {
+        opacity: 0,
+        x: -50,
+        y: 50,
+        duration: 1,
+        ease: 'power3.out',
+        scrollTrigger: {
+            trigger: '.see',
+            start: 'top 80%',
+            scrub: true, // Adjust the start position as needed
+        },
+    });
+
+    var footerAnimation = gsap.from('.footer, .icon', {
+        opacity: 0,
+        y: 100,
+        duration: 2,
+        ease: 'power3.out',
+        scrollTrigger: {
+            trigger: '.footer',
+            start: 'top 80%',
+            scrub: true, // Adjust the start position as needed
+        },
+    });
+
+    // ScrollTrigger refresh to update the scroll positions
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.refresh();
+});
